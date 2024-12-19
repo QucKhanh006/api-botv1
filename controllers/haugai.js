d06b6bb81db3b9c4f8558946d1ba3c99c040c72c
QucKhanh006 <quockhanh200555@gmail.com>
2024-12-19 20:12:26 +0700

    Update haugai.js

--- controllers/haugai.js
+++ controllers/haugai.js
@@ -4,7 +4,7 @@ exports.haugai = async (req, res, next) => {
   var data = require('fs-extra').readFileSync(__dirname + '/../haugai.txt', 'utf-8').split('\n');
   link = data[Math.floor(Math.random() * data.length)].trim();
   link1 = data[Math.floor(Math.random() * data.length)].trim();
-  res.jsonp({"Authors":"Đức tài cuti vcl",
+  res.jsonp({"Authors":"Khánh cuti vcl",
              "url":`${link}`,
              "data":`${link1}`});
 };
